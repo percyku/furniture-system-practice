9dfb29e1df524e0243e2fdbe589dfe87149e2a60
Percy.Ku.Dev1 <percyku19@gmail.com>
2025-11-09 21:11:51 +0800

    finish V1

--- admin.js
+++ admin.js
@@ -2,22 +2,276 @@ const baseUrl = "https://livejs-api.hexschool.io/api/livejs/v1/admin/";
 const api_path = "percyku19api";
 const token = "KDdHk6jfkCPVofZFXJVGHm7CCbg2";
 
-// C3.js
-let chart = c3.generate({
-  bindto: "#chart", // HTML 元素綁定
-  data: {
-    type: "pie",
-    columns: [
-      ["Louvre 雙人床架", 1],
-      ["Antony 雙人床架", 2],
-      ["Anty 雙人床架", 3],
-      ["其他", 4],
-    ],
-    colors: {
-      "Louvre 雙人床架": "#DACBFF",
-      "Antony 雙人床架": "#9D7FEA",
-      "Anty 雙人床架": "#5434A7",
-      其他: "#301E5F",
-    },
-  },
+let recordOrder = [];
+
+const orderBody = document.querySelector(".orderPage-table tbody");
+
+const discardAllBtn = document.querySelector(".discardAllBtn");
+
+discardAllBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (recordOrder.length === 0) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Order items are empty",
+    });
+    return;
+  }
+
+  disAllOrderListBtn();
+
+  axios
+    .delete(`${baseUrl}${api_path}/orders`, {
+      headers: {
+        Authorization: token,
+      },
+    })
+    .then((res) => {
+      discardAllBtn.removeAttribute("disabled");
+      renderOrder(res.data.orders);
+    })
+    .catch((error) => {
+      discardAllBtn.removeAttribute("disabled");
+      console.log(error);
+    });
 });
+
+async function getOrderList() {
+  return axios
+    .get(`${baseUrl}${api_path}/orders`, {
+      headers: {
+        Authorization: token,
+      },
+    })
+    .then((res) => {
+      return res.data.orders;
+    })
+    .catch((error) => {});
+}
+
+async function renderOrder(orders) {
+  recordOrder = orders;
+  renderCart(orders);
+  orderBody.innerHTML = orders
+    .map(
+      (order) =>
+        `
+            <tr>
+              <td>${order.id}</td>
+              <td>
+                <p>${order.user.name}</p>
+                <p>${order.user.tel}</p>
+              </td>
+              <td>${order.user.address}</td>
+              <td>${order.user.email}</td>
+              <td>
+                ${order.products
+                  .map((product) => `<p>${product.title}</p>`)
+                  .join("")}
+              </td>
+              <td>${new Date(order.createdAt * 1000)
+                .toLocaleDateString()
+                .replaceAll("/", "-")}</td>
+              <td class="orderStatus" >
+                <a href="#"  data-id="${order.id}">${
+          order.paid === true ? "已處理" : "未處理"
+        }</a>
+              </td>
+              <td>
+                <input type="button" class="delSingleOrder-Btn" disabled ="true" value="刪除"  data-id="${
+                  order.id
+                }"/>
+              </td>
+            </tr>
+    `
+    )
+    .join("");
+
+  try {
+    await renderorderStatus();
+    await renderDeleteSingleOrderItem();
+  } catch (error) {
+    console.log(error);
+    init();
+  } finally {
+    enableAllOrderListBtn();
+  }
+}
+
+function renderCart(orders) {
+  let totalNum = {};
+  let finalData = [];
+  let otherTotalNum = {};
+
+  orders.forEach((order) => {
+    order.products.forEach((product) => {
+      if (totalNum[product.title] === undefined) {
+        totalNum[product.title] = 1;
+      } else {
+        totalNum[product.title] += 1;
+      }
+    });
+  });
+
+  const sortedData = Object.entries(totalNum);
+
+  sortedData.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
+  let colorCode = ["#DACBFF", "#9D7FEA", "#5434A7", "#301E5F"];
+
+  let color = {};
+
+  sortedData.forEach((item, i) => {
+    if (i < 3) {
+      color[sortedData[i][0]] = colorCode[i];
+      finalData[i] = item;
+    } else if ((i = 3)) {
+      color["其他"] = colorCode[3];
+    }
+
+    if (i >= 3) {
+      if (otherTotalNum["其他"] === undefined) {
+        otherTotalNum["其他"] = parseInt(sortedData[i][1]);
+      } else {
+        otherTotalNum["其他"] += parseInt(sortedData[i][1]);
+      }
+    }
+  });
+
+  if (otherTotalNum["其他"] !== undefined) {
+    finalData[3] = Object.entries(otherTotalNum)[0];
+  }
+
+  c3.generate({
+    bindto: "#chart", // HTML 元素綁定
+    data: {
+      type: "pie",
+      columns: finalData,
+      colors: color,
+    },
+  });
+}
+
+async function renderorderStatus() {
+  let orderStatus = document.querySelectorAll(".orderStatus");
+  orderStatus.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      let status = e.target.text === "已處理" ? false : true;
+
+      disAllOrderListBtn();
+      editOrderItemStautAndRenderOrders(e.target.dataset.id, status);
+    });
+  });
+}
+
+async function renderDeleteSingleOrderItem() {
+  let delSingleOrderBtns = document.querySelectorAll(".delSingleOrder-Btn");
+  delSingleOrderBtns.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      disAllOrderListBtn();
+      deleteOrderItemAndRenderOrders(e.target.dataset.id);
+    });
+  });
+}
+
+function editOrderItemStautAndRenderOrders(orderId, status) {
+  axios
+    .put(
+      `https://livejs-api.hexschool.io/api/livejs/v1/admin/${api_path}/orders`,
+      {
+        data: {
+          id: orderId,
+          paid: status,
+        },
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    )
+    .then((res) => {
+      renderOrder(res.data.orders);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function deleteOrderItemAndRenderOrders(orderId) {
+  axios
+    .delete(`${baseUrl}${api_path}/orders/${orderId}`, {
+      headers: {
+        Authorization: token,
+      },
+    })
+    .then((res) => {
+      renderOrder(res.data.orders);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function disAllOrderListBtn() {
+  document.querySelectorAll(".orderStatus").forEach((item) => {
+    item.children[0].setAttribute("class", "disabled");
+  });
+
+  document.querySelectorAll(".delSingleOrder-Btn").forEach((item) => {
+    item.setAttribute("disabled", true);
+  });
+
+  discardAllBtn.setAttribute("disabled", true);
+}
+
+function enableAllOrderListBtn() {
+  document.querySelectorAll(".orderStatus").forEach((item) => {
+    item.children[0].removeAttribute("class");
+  });
+
+  document.querySelectorAll(".delSingleOrder-Btn").forEach((item) => {
+    item.removeAttribute("disabled");
+  });
+
+  discardAllBtn.removeAttribute("disabled");
+}
+
+async function init() {
+  try {
+    let orders = await getOrderList();
+    renderOrder(orders);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
+
+// C3.js
+// let chart = c3.generate({
+//   bindto: "#chart", // HTML 元素綁定
+//   data: {
+//     type: "pie",
+//     columns: [
+//       ["Louvre 雙人床架", 1],
+//       ["Antony 雙人床架", 2],
+//       ["Anty 雙人床架", 3],
+//       ["其他", 4],
+//     ],
+//     colors: {
+//       "Louvre 雙人床架": "#DACBFF",
+//       "Antony 雙人床架": "#9D7FEA",
+//       "Anty 雙人床架": "#5434A7",
+//       其他: "#301E5F",
+//     },
+//   },
+// });
--- app.js
+++ app.js
@@ -1,3 +1,431 @@
-const baseUrl = "https://livejs-api.hexschool.io/api/livejs/v1/admin/";
+const baseUrl = "https://livejs-api.hexschool.io/api/livejs/v1/customer/";
 const api_path = "percyku19api";
 const token = "KDdHk6jfkCPVofZFXJVGHm7CCbg2";
+
+const constraints = {
+  姓名: {
+    presence: {
+      message: "是必填欄位",
+    },
+  },
+  電話: {
+    presence: {
+      message: "是必填欄位",
+    },
+    length: {
+      minimum: 8,
+      message: "號碼需超過 8 碼",
+    },
+  },
+  信箱: {
+    presence: {
+      message: "是必填欄位",
+    },
+    email: {
+      message: "格式有誤",
+    },
+  },
+  寄送地址: {
+    presence: {
+      message: "是必填欄位",
+    },
+  },
+};
+
+let recordProducts = [];
+let recordCart = [];
+
+const productList = document.querySelector(".productWrap");
+const productSelect = document.querySelector(".productSelect");
+productSelect.addEventListener("change", (e) => {
+  let category = e.target.value;
+  if (category === "全部") {
+    renderProduct(recordProducts);
+    return;
+  }
+
+  let targetProducts = recordProducts.filter((item) => {
+    if (item.category === category) {
+      return item;
+    }
+  });
+
+  renderProduct(targetProducts);
+});
+
+async function getProducts() {
+  return axios
+    .get(`${baseUrl}${api_path}/products`)
+    .then((res) => {
+      return res.data.products;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function renderProduct(products) {
+  productList.innerHTML = products
+    .map(
+      (item) => `
+       <li class="productCard">
+          <h4 class="productType">新品</h4>
+          <img
+            src="${item.images}"
+            alt="${item.title},${item.category}"
+          />
+          <button href="#" class="addCardBtn" data-id="${item.id}">加入購物車</button>
+          <h3>${item.title}</h3>
+          <del class="originPrice">NT$${item.origin_price}</del>
+          <p class="nowPrice">NT$${item.price}</p>
+        </li>
+    `
+    )
+    .join("");
+
+  let addCartBtn = document.querySelectorAll(".addCardBtn");
+  addCartBtn.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      addCartItem(e.target.dataset.id);
+      // console.log("end2");
+    });
+  });
+}
+
+async function addCartItem(productId, quantity = 1) {
+  let qty = 0;
+  try {
+    disableCartAllBtn();
+    disabelShoppingItemBtn();
+    let myCart = await getCart();
+    qty = myCart.carts.find((item) => item.product.id === productId)?.quantity;
+    qty = qty == undefined ? 0 : qty;
+  } catch (error) {
+    console.log(error);
+  }
+
+  enableCartAllBtn();
+  enableShoppingItemBtn();
+
+  axios
+    .post(
+      `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`,
+      {
+        data: {
+          productId: productId,
+          quantity: qty + 1,
+        },
+      }
+    )
+    .then(function (response) {
+      renderCart(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function getCategories(products) {
+  let sorted = products
+    .map((item) => item.category)
+    .reduce((pre, cur) => {
+      if (pre.includes(cur) == false) {
+        pre.push(cur);
+      }
+      return pre;
+    }, []);
+
+  renderCategories(sorted);
+}
+
+function renderCategories(sorted) {
+  let str = '<option value="全部" selected>全部</option>';
+  sorted.forEach(function (item) {
+    str += `<option value="${item}">${item}</option>`;
+  });
+  productSelect.innerHTML = str;
+}
+
+const shoppingCart = document.querySelector(".shoppingCart tbody");
+const finalPrice = document.querySelector(".finalPrice");
+async function getCart() {
+  return axios
+    .get(`${baseUrl}${api_path}/carts`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      res.data = [];
+    });
+}
+
+async function renderCart(carts) {
+  if (carts.carts.length === 0) {
+    recordCart = [];
+    discardAllBtn.setAttribute("disabled", true);
+  } else {
+    recordCart = carts.carts;
+    discardAllBtn.removeAttribute("disabled");
+  }
+  finalPrice.innerHTML = carts.finalTotal;
+  shoppingCart.innerHTML = carts.carts
+    .map(
+      (item) => `
+          <tr>
+              <td>
+                <div class="cardItem-title">
+                  <img src="${item.product.images}" alt="" />
+                  <p>${item.product.title}</p>
+                </div>
+              </td>
+              <td>NT$${item.product.origin_price}</td>
+              <td class="">
+                <div class="quality-control">
+
+
+                  <button class="add-btn">
+                    <span class="material-icons"  data-id="${
+                      item.id
+                    }"> add </span>
+                  </button>
+                  <span class="a-num" >${item.quantity}</span>
+                  <button class="remove-btn"  ${
+                    item.quantity === 1 ? "disabled='true'" : ""
+                  }>
+                    <span class="material-icons"  data-id="${
+                      item.id
+                    }"> remove </span>
+                  </button>
+                </div>
+              </td>
+              <td>NT$${item.product.price}</td>
+              <td class="discardBtn">
+                <button class="material-icons" data-id="${
+                  item.id
+                }"> clear </button>
+              </td>
+            </tr>
+    `
+    )
+    .join("");
+
+  try {
+    await renderDiscardBtn();
+    await renderAddBtn();
+    await renderRemoveBtn();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    enableShoppingItemBtn();
+  }
+}
+
+async function renderAddBtn() {
+  let addBtn = document.querySelectorAll(".add-btn");
+  addBtn.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      let qty =
+        parseInt(e.target.parentElement.nextElementSibling.innerText) + 1;
+      disableCartAllBtn();
+      disabelShoppingItemBtn();
+      modifyOrderProductQtyAndRenderCart(e.target.dataset.id, qty);
+    });
+  });
+}
+
+async function renderRemoveBtn() {
+  let addBtn = document.querySelectorAll(".remove-btn");
+  addBtn.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      let qty =
+        parseInt(e.target.parentElement.previousElementSibling.innerText) - 1;
+      disableCartAllBtn();
+      disabelShoppingItemBtn();
+      modifyOrderProductQtyAndRenderCart(e.target.dataset.id, qty);
+    });
+  });
+}
+
+function modifyOrderProductQtyAndRenderCart(productId, qty) {
+  axios
+    .patch(`${baseUrl}${api_path}/carts`, {
+      data: {
+        id: productId,
+        quantity: qty,
+      },
+    })
+    .then((res) => {
+      renderCart(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+async function renderDiscardBtn() {
+  let discardBtn = document.querySelectorAll(".discardBtn");
+  discardBtn.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      disableCartAllBtn();
+      disabelShoppingItemBtn();
+
+      axios
+        .delete(`${baseUrl}${api_path}/carts/${e.target.dataset.id}`)
+        .then((res) => {
+          renderCart(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  });
+}
+
+const discardAllBtn = document.querySelector(".discardAllBtn");
+discardAllBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  disableCartAllBtn();
+
+  axios
+    .delete(`${baseUrl}${api_path}/carts`)
+    .then((res) => {
+      renderCart(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+function disableCartAllBtn() {
+  document.querySelectorAll(".shoppingCart button").forEach((item) => {
+    item.setAttribute("disabled", true);
+  });
+}
+
+function disabelShoppingItemBtn() {
+  document.querySelectorAll(".addCardBtn").forEach((item) => {
+    item.setAttribute("disabled", true);
+  });
+}
+
+function enableCartAllBtn() {
+  document.querySelectorAll(".shoppingCart button").forEach((item) => {
+    item.removeAttribute("disabled");
+  });
+}
+
+function enableShoppingItemBtn() {
+  document.querySelectorAll(".addCardBtn").forEach((item) => {
+    item.removeAttribute("disabled");
+  });
+}
+
+async function init() {
+  try {
+    let productList = await getProducts();
+    recordProducts = productList;
+    renderProduct(productList);
+    getCategories(productList);
+
+    let myCart = await getCart();
+    renderCart(myCart);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
+
+const form = document.querySelector(".orderInfo-form");
+const orderInfoBtn = document.querySelector(".orderInfo-btn");
+
+const inputs = document.querySelectorAll(
+  "input[type=text],input[type=tel],input[type=email]"
+);
+
+const messages = document.querySelectorAll("[data-message]");
+
+form.addEventListener("submit", verification, false);
+function verification(e) {
+  e.preventDefault();
+  if (recordCart.length === 0) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Shopping cart items are empty",
+    });
+    return;
+  }
+  let errors = validate(form, constraints);
+  if (errors) {
+    showErrors(errors);
+  } else {
+    addOrder();
+  }
+
+  // addOrder();
+}
+
+function showErrors(errors) {
+  messages.forEach((item) => {
+    item.textContent = "";
+    item.textContent = errors[item.dataset.message];
+  });
+}
+// 監控所有 input 的操作
+inputs.forEach((item) => {
+  item.addEventListener("change", function (e) {
+    e.preventDefault();
+    let targetName = item.name;
+    let errors = validate(form, constraints);
+    item.nextElementSibling.textContent = "";
+    // 針對正在操作的欄位呈現警告訊息
+    if (errors) {
+      document.querySelector(`[data-message='${targetName}']`).textContent =
+        errors[targetName];
+    }
+  });
+});
+
+function addOrder() {
+  orderInfoBtn.setAttribute("disabled", true);
+  disableCartAllBtn();
+  let url = `${baseUrl}${api_path}/orders`;
+  let data = {
+    data: {
+      user: {
+        name: document.querySelector("#customerName").value.trim(),
+        tel: document.querySelector("#customerPhone").value.trim(),
+        email: document.querySelector("#customerEmail").value.trim(),
+        address: document.querySelector("#customerAddress").value.trim(),
+        payment: document.querySelector("#tradeWay").value.trim(),
+        // name: "testSser",
+        // tel: "123456789",
+        // email: "test@gmail.com",
+        // address: "test123",
+        // payment: "信用卡",
+      },
+    },
+  };
+
+  axios
+    .post(url, data)
+    .then((res) => {
+      data = {};
+      init();
+      orderInfoBtn.removeAttribute("disabled");
+      form.reset();
+    })
+    .catch((error) => {
+      console.log(error);
+      enableCartAllBtn();
+      enableShoppingItemBtn();
+      orderInfoBtn.removeAttribute("disabled");
+    });
+}
